684fe20d8e08ba33d4606dbf47cf3661a1b03aa3
Elshad Agayev <elshad@cryptomessage.io>
2018-04-05 17:00:57 -0700

    commit

--- app/models/Rooms.js
+++ app/models/Rooms.js
@@ -77,6 +77,62 @@ module.exports = (mongoose) => {
         })
     }
 
+    const addSong = (roomToken, clientID, songID, callback) => {
+        roomToken = roomToken.replace(' ', '+');
+        clientID = clientID.replace(' ', '+');
+        songID = songID.replace(' ', '+');
+
+        model.findOne({
+            token: roomToken
+        }).then(res => {
+            const clients = res.clients instanceof Array ? res.clients : [];
+            const songs = res.songs instanceof Array ? res.songs : [];
+            client = clients.find(el => {
+                return el.clientID === clientID
+            })
+
+            if(!client)
+                throw "You have no access to this room"
+            if(!songs.find(el => {
+                return el.songID === songID
+            })) {
+                songs.push({
+                    clientID,
+                    songID,
+                    likes: 0,
+                    dislikes: 0,
+                });
+            }
+
+            model.findByIdAndUpdate(res._id, {
+                songs
+            }).then(res => {
+                callback(null, res);
+            }).catch(err => {
+                callback(err, null);
+            })
+        })
+    }
+
+    const getClientSongs = (roomToken, clientID, callback) => {
+        console.log("AAA", roomToken, clientID)
+        model.findOne({
+            token: roomToken
+        }).then(res => {
+            if(!res)
+                return callback("Could not find songs", null)
+            let songs = res.songs;
+            songs = songs.map(el => {
+                if(el.clientID === clientID)
+                    return el;
+            })
+
+            callback(null, songs);
+        }).catch(err => {
+            callback(err, null);
+        })
+    }
+
     const save = (obj) => {
         return new model(obj);
     }
@@ -85,6 +141,8 @@ module.exports = (mongoose) => {
         authClient,
         save,
         findRooms,
-        findRoom
+        findRoom,
+        addSong,
+        getClientSongs
     }
 }
--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -22,133 +22,181 @@ class ApiRoute extends API {
     createClientRoutes () {
         this.app.post('/api/client/auth', (req, res) => this.authClient(req, res))
         this.app.get('/api/client/songs', (req, res) => this.getClientSongs(req, res))
+        this.app.post('/api/client/songs/add', (req, res) => this.addClientSong(req, res))
+    }
+
+    addClientSong(req, res) {
+        try {
+            const { songID, token, clientID } = req.body;
+            Rooms.addSong(token, clientID, songID, (err, resp) => {
+                if(err)
+                    return res.json(this.errorResponse("Could not add song"))
+                res.json(this.successResponse("OK"));
+            });
+        } catch(E) {
+            res.json(this.errorResponse(E))
+        }
     }
 
     getClientSongs (req, res) {
-        const { token, clientID } = req.query;
+        try {
+            const { token, clientID } = req.query;
 
-        res.json(this.successResponse(req.query))
+            Rooms.getClientSongs(token, clientID, (err, resp) => {
+                if(err)
+                    return res.json(this.errorResponse("Could not get songs" + err))
+                res.json(this.successResponse(resp));
+            })
+        } catch (E) {
+            res.json(this.errorResponse(E))
+        }
     }
 
     createRoom (req, res) {
-        const { password, name, dj } = req.body;
+        try {
+            const { password, name, dj } = req.body;
 
-        if(!password || !password.trim())
-            return res.json(this.errorResponse("Password is required"))
+            if(!password || !password.trim())
+                return res.json(this.errorResponse("Password is required"))
 
-        DJ.findOne({
-            token: dj
-        }).then((resp) => {
-            const room = Rooms.save({
-                password, name, dj: resp.id
+            DJ.findOne({
+                token: dj
+            }).then((resp) => {
+                const room = Rooms.save({
+                    password, name, dj: resp.id
+                }).save((err, resp) => {
+                    if(err)
+                        switch(err.code) {
+                            case 11000:
+                                return res.json(this.errorResponse("This room exists in our database. Type another email"))
+                            default:
+                                return res.json(this.errorResponse(err.errmsg))
+                        }
+                        
+                        res.json(this.successResponse({
+                            id: resp._id,
+                            token: resp.token
+                        }))
+                })
+            }).catch(err => {
+                res.json(this.errorResponse(err))
+            })
+        } catch(E) {
+            res.json(this.errorResponse(E));
+        }
+    }
+
+    getRoom (req, res) {
+        try {
+            let { room_id, token } = req.query;
+            token = token ? token.replace(' ', '+') : "";
+            
+            if(!token)
+                return res.json(this.successResponse([]));
+            
+            DJ.findOne({
+                token
+            }).then(resp => {
+                Rooms.findRoom(room_id).then(resp => {
+                    res.json(this.successResponse(resp));
+                }).catch(err => {
+                    res.json(this.errorResponse(err));
+                });
+            }).catch(err => {
+                res.json(this.errorResponse(err))
+            })
+        } catch(E) {
+            res.json(this.errorResponse(E))
+        }
+    }
+
+    getRooms (req, res) {
+        try {
+            let { token } = req.query;
+            token = token ? token.replace(' ', '+') : "";
+            if(!token)
+                return res.json(this.successResponse([]));
+
+            DJ.findOne({
+                token
+            }).then(resp => {
+                Rooms.findRooms({
+                    dj: resp._id
+                }).then(resp => {
+                    res.json(this.successResponse(resp));
+                }).catch(err => {
+                    res.json(this.errorResponse(err));
+                });
+            }).catch(err => {
+                res.json(this.errorResponse(err))
+            })
+        } catch (E) {
+            res.json(this.errorResponse(E))
+        }
+    }
+
+    registerDJ (req, res) {
+        try {
+            const { email, password, retypePassword } = req.body;
+
+            if(!password.trim())
+                return res.json(this.errorResponse("Password is required"))
+            
+            if(password !== retypePassword)
+                return res.json(this.errorResponse("Retype password correctly"))
+
+            const dj = DJ.save({
+                email,
+                password
             }).save((err, resp) => {
                 if(err)
                     switch(err.code) {
                         case 11000:
-                            return res.json(this.errorResponse("This room exists in our database. Type another email"))
+                            return res.json(this.errorResponse("This email exists in our database. Type another email"))
                         default:
                             return res.json(this.errorResponse(err.errmsg))
                     }
                     
                     res.json(this.successResponse({
                         id: resp._id,
+                        email: resp.email,
                         token: resp.token
                     }))
             })
-        }).catch(err => {
-            res.json(this.errorResponse(err))
-        })
-    }
-
-    getRoom (req, res) {
-        let { room_id, token } = req.query;
-        token = token ? token.replace(' ', '+') : "";
-        
-        if(!token)
-            return res.json(this.successResponse([]));
-        
-        DJ.findOne({
-            token
-        }).then(resp => {
-            Rooms.findRoom(room_id).then(resp => {
-                res.json(this.successResponse(resp));
-            }).catch(err => {
-                res.json(this.errorResponse(err));
-            });
-        }).catch(err => {
-            res.json(this.errorResponse(err))
-        })
-    }
-
-    getRooms (req, res) {
-        let { token } = req.query;
-        token = token ? token.replace(' ', '+') : "";
-        if(!token)
-            return res.json(this.successResponse([]));
-
-        DJ.findOne({
-            token
-        }).then(resp => {
-            Rooms.findRooms({
-                dj: resp._id
-            }).then(resp => {
-                res.json(this.successResponse(resp));
-            }).catch(err => {
-                res.json(this.errorResponse(err));
-            });
-        }).catch(err => {
-            res.json(this.errorResponse(err))
-        })
-    }
-
-    registerDJ (req, res) {
-        const { email, password, retypePassword } = req.body;
-
-        if(!password.trim())
-            return res.json(this.errorResponse("Password is required"))
-        
-        if(password !== retypePassword)
-            return res.json(this.errorResponse("Retype password correctly"))
-
-        const dj = DJ.save({
-            email,
-            password
-        }).save((err, resp) => {
-            if(err)
-                switch(err.code) {
-                    case 11000:
-                        return res.json(this.errorResponse("This email exists in our database. Type another email"))
-                    default:
-                        return res.json(this.errorResponse(err.errmsg))
-                }
-                
-                res.json(this.successResponse({
-                    id: resp._id,
-                    email: resp.email,
-                    token: resp.token
-                }))
-        })
+        } catch(E) {
+            res.json(this.successResponse(E))
+        }
     }
 
     authDJ (req, res) {
-        const { email, password } = req.body;
-        DJ.auth({
-            email, password
-        }).then(resp => {
-            res.json(this.successResponse(resp))
-        }).catch(err => {
-            res.json(this.errorResponse("Email or password is not correct"))
-        });
+        try {
+            const { email, password } = req.body;
+            DJ.auth({
+                email, password
+            }).then(resp => {
+                if(!resp)
+                    return res.json(this.errorResponse("Email or password is not correct"))
+                res.json(this.successResponse(resp))
+            }).catch(err => {
+                res.json(this.errorResponse("Email or password is not correct"))
+            });
+        }catch(E) {
+            res.json(this.errorResponse(E))
+        }
     }
 
     authClient (req, res) {
-        const { password, full_name } = req.body;
-        Rooms.authClient(password, full_name, (err, resp) => {
-            if(err)
-                return res.json(this.errorResponse(err))
-            
-            res.json(this.successResponse(resp))
-        });
+        try {
+            const { password, full_name } = req.body;
+            Rooms.authClient(password, full_name, (err, resp) => {
+                if(err)
+                    return res.json(this.errorResponse(err))
+                
+                res.json(this.successResponse(resp))
+            })
+        } catch (E) {
+            res.json(this.errorResponse("Password is incorrect. Try it again"))
+        }
     }
 
 }
